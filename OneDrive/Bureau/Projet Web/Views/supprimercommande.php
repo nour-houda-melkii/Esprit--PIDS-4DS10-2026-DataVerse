b9e6c52fab4aa5d885abc80f63547656b595ea03
nour houda melki <nourmelki05@gmail.com>
2026-04-06 10:03:52 +0100

    Initial empty commit

--- OneDrive/Bureau/Projet Web/Views/supprimercommande.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "../../Core/commandecore.php";
-$commandecore=new commandecore();
-if (isset($_POST["id_Commande"])){
-	$commandecore->supprimercommande($_POST["id_Commande"]);
-	header('Location: checkout.php');
-}
-
-?>
